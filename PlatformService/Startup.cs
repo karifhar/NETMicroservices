0cb87868aeaa13949a7cd8f431d692029b1674ce
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-12-29 09:58:26 +0700

    menambahkan seeding

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -30,7 +30,8 @@ namespace PlatformService
         {
             services.AddDbContext<AppDbContext>(
                 opt=>opt.UseInMemoryDatabase("InMem"));
-                
+            
+            services.AddScoped<IPlatformRepo,PlatformRepo>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -58,6 +59,8 @@ namespace PlatformService
             {
                 endpoints.MapControllers();
             });
+
+            PrepDb.PrepPopulation(app);
         }
     }
 }
